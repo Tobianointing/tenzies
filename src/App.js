a8aa4097d263f903eb0cf23c329099d11eed9597
anoindevdev <anoindevdev@gmail.com>
2022-06-02 19:17:10 +0100

    finished v1

--- src/App.js
+++ src/App.js
@@ -1,113 +1,119 @@
-import Main from "./components/Main";
-import React from "react";
-import Die from "./components/Die";
-import uuid from "react-uuid";
-import Confetti from "react-confetti";
+import Main from "./components/Main"
+import React from "react"
+import Die from "./components/Die"
+import uuid from "react-uuid"
+import Confetti from "react-confetti"
 
 function App() {
-  const [diceArray, setDiceArray] = React.useState(makeNewDice());
-  const [gameStatus, setGameStatus] = React.useState(false);
-  const [rollNumbers, setRollNumbers] = React.useState(0);
-  const [rollNumbersArray, setRollNumbersArray] = React.useState([]);
+  const [diceArray, setDiceArray] = React.useState(makeNewDice())
+  const [gameStatus, setGameStatus] = React.useState(false)
+  const [rollNumbers, setRollNumbers] = React.useState(0)
+  const [bestScore, setBestScore] = React.useState(getBestScore())
+
+  function getBestScore() {
+    if (localStorage.getItem("gameRolls")) {
+      return Math.min(...JSON.parse(localStorage.getItem("gameRolls")))
+    } else {
+      return ""
+    }
+  }
 
   React.useEffect(() => {
-    const firstVal = diceArray[0].num;
+    const firstVal = diceArray[0].num
     const allHold = diceArray.every((die) => {
-      return die.isHold === true;
-    });
+      return die.isHold === true
+    })
     const allEqual = diceArray.every((die) => {
-      return die.num === firstVal;
-    });
+      return die.num === firstVal
+    })
 
     if (allHold && allEqual) {
-      setGameStatus(true);
-      setRollNumbersArray((prevState) => [...prevState, rollNumbers]);
+      setGameStatus(true)
     }
-  }, [diceArray, rollNumbers]);
+  }, [diceArray])
 
   React.useEffect(() => {
-    const gameRollsFromStorage = JSON.parse(localStorage.getItem("gameRolls"));
-    localStorage.setItem(
-      "gameRolls",
-      JSON.stringify([
-        ...new Set(gameRollsFromStorage),
-        ...new Set(rollNumbersArray),
-      ])
-    );
-  }, [rollNumbersArray]);
+    if (gameStatus) {
+      const gameRollsFromStorage =
+        JSON.parse(localStorage.getItem("gameRolls")) || []
+
+      localStorage.setItem(
+        "gameRolls",
+        JSON.stringify([...gameRollsFromStorage, rollNumbers])
+      )
+
+      setBestScore(Math.min(...gameRollsFromStorage, rollNumbers))
+    }
+  }, [gameStatus, rollNumbers])
 
   function rollDice() {
     if (gameStatus) {
-      setGameStatus(false);
-      setRollNumbers(0);
-      setDiceArray(makeNewDice());
+      setGameStatus(false)
+      setRollNumbers(0)
+      setDiceArray(makeNewDice())
     } else {
-      setRollNumbers((prevNum) => prevNum + 1);
-
       const rollDiceArray = diceArray.map((die) => {
         if (die.isHold) {
-          return die;
+          return die
         } else {
-          let id = uuid();
-          let num = Math.floor(Math.random() * 6) + 1;
+          let id = uuid()
+          let num = Math.floor(Math.random() * 6) + 1
           return {
             id: id,
             num,
             isHold: false,
-          };
+          }
         }
-      });
+      })
 
-      setDiceArray(rollDiceArray);
+      setDiceArray(rollDiceArray)
+      setRollNumbers((prevNum) => prevNum + 1)
     }
   }
 
   function makeNewDice() {
-    let numArray = [];
+    let numArray = []
     for (let i = 0; i < 10; i++) {
-      let id = uuid();
-      let num = Math.floor(Math.random() * 6) + 1;
+      let id = uuid()
+      let num = Math.floor(Math.random() * 6) + 1
       numArray.push({
         id: id,
         num,
         isHold: false,
-      });
+      })
     }
 
-    return numArray;
+    return numArray
   }
 
   function handleToggleHold(id) {
     const newToggleArray = diceArray.map((die) => {
       if (die.id === id) {
-        return { ...die, isHold: !die.isHold };
+        return { ...die, isHold: !die.isHold }
       } else {
-        return die;
+        return die
       }
-    });
+    })
 
-    setDiceArray(newToggleArray);
-  }
-
-  function handleRollDice() {
-    rollDice();
+    setDiceArray(newToggleArray)
   }
 
   const dices = diceArray.map((die) => (
     <Die toggleHold={handleToggleHold} key={die.id} die={die} />
-  ));
+  ))
 
   return (
     <div className="App">
       {gameStatus && <Confetti />}
       <Main
         dices={dices}
-        rollDice={handleRollDice}
+        rollDice={rollDice}
         gameStatus={gameStatus}
         rollNumbers={rollNumbers}
+        bestScore={bestScore}
       />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Main.js
+++ src/components/Main.js
@@ -8,14 +8,18 @@ export default function Main(props) {
           Roll until all dice are the same. Click each die to freeze it at its
           current value between rolls.
         </p>
-        <p>Roll numbers: {props.rollNumbers}</p>
+        <p>No. of rolls: {props.rollNumbers}</p>
+
+        {props.bestScore && (
+          <p className="best--score">Best Score: {props.bestScore}</p>
+        )}
 
         <div className="dice--wrapper">{props.dices}</div>
 
-        <button className="game--button" onClick={props.rollDice}>
+        <button className="game--button" onClick={() => props.rollDice()}>
           {props.gameStatus ? "New Game" : "Roll"}
         </button>
       </div>
     </main>
-  );
+  )
 }
